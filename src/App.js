67a575a52292882d3fd346861207c2dc57aee9ad
Alan-Miller <alan.ashman@gmail.com>
2017-10-28 22:38:28 -0600

    App makes and shuffles deck and deals to players

--- src/App.js
+++ src/App.js
@@ -1,16 +1,97 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      deck: this.shuffle(this.makeDeck()),
+      playerOneHand: [],
+      playerTwoHand: [],
+      playerThreeHand: [],
+      playerFourHand: []
+    }
+  }
+
+  componentDidMount() {
+    this.deal(this.state.deck);
+  }
+
+  makeDeck() {
+    return Array.from(Array(52).keys()).map((item, i) => {
+      const id = i + 1;
+      const value = [ 'Joker', 'A', '2', '3', '4', '5', '6', '7', 
+                      '8', '9', '10', 'J', 'Q', 'K'][(i % 13) + 1];
+      const suit = id < 14 ? '♣' 
+          : id < 27 ? '♦' 
+          : id < 40 ? '♠' 
+          : '♥';
+      return {id, value, suit};
+    }); 
+  }
+
+  shuffle(deck) {
+    let shuffledDeck = [];
+    while (deck.length) {
+      const i = ~~(Math.random() * deck.length);
+      shuffledDeck.push(deck.splice(i, 1)[0]);
+    }
+    return shuffledDeck;
+  }
+
+  deal(deck) {
+    const { playerOneHand, playerTwoHand, playerThreeHand, playerFourHand } = this.state;
+    while (deck.length) {
+      const round = deck.splice(0, 4);
+      playerOneHand.push(round[0]);
+      playerTwoHand.push(round[1]);
+      playerThreeHand.push(round[2]);
+      playerFourHand.push(round[3]);
+      this.setState({ playerOneHand, playerTwoHand, playerThreeHand, playerFourHand });
+    }
+    console.log('hands', playerOneHand, playerTwoHand, playerThreeHand, playerFourHand);
+  }
+
   render() {
+    const { playerOneHand, playerTwoHand, playerThreeHand, playerFourHand } = this.state;
     return (
       <div className="App">
-        <div className="sidebar">
-        </div>
+        <div className="sidebar"></div>
         <div className="table">
-          <div className="player one computer"></div>
-          <div className="player two computer"></div>
-          <div className="player three computer"></div>
-          <div className="player four human"></div>
+
+          <div className="player one computer">
+            <div className="hand">
+              { playerOneHand.map((card, i) => (
+                <div className="card" key={i}>{card.value}{card.suit}</div>
+              )) }
+            </div>
+          </div>
+          <div className="player two computer">
+            <div className="hand">
+              { playerTwoHand.map((card, i) => (
+                <div className="card" key={i}>{card.value}{card.suit}</div>
+              )) }
+            </div>
+          </div>
+          <div className="player three computer">
+            <div className="hand">
+              { playerThreeHand.map((card, i) => (
+                <div className="card" key={i}>{card.value}{card.suit}</div>
+              )) }
+            </div>
+          </div>
+          <div className="player four human">
+            <div className="hand">
+              { playerFourHand.map((card, i) => (
+                <div className="card" key={i}>{card.value}{card.suit}</div>
+              )) }
+            </div>
+          </div>
+          
+          <div className="centerOfTable">
+            <div className="discard"></div>
+          </div>
         </div>
       </div>
     );
@@ -18,3 +99,5 @@ class App extends Component {
 }
 
 export default App;
+
+
